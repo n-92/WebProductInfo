f2202db0b8fe001d35720f1798858e1ee87aafe8
Naing <au.naingoo@gmail.com>
2018-01-11 15:56:56 +0800

    First upload

---
+++ WebApplication1/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ProductWebAPI.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.Clear();
+            config.Routes.MapHttpRoute(
+               name: "DefaultApi",
+               routeTemplate: "api/v1/{controller}/{id}",
+               defaults: new{ id = RouteParameter.Optional }
+            );
+        }
+    }
+}
